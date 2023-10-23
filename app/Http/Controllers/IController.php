63e1ec920178d3e037b1e388d9da2ef989e15d5e
Himanshu Sharma <142489823+Himanshusharma47@users.noreply.github.com>
2023-10-23 16:43:58 +0530

    files added

---
+++ app/Http/Controllers/IController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\City;
+use App\Models\State;
+use App\Models\Country;
+
+
+class IController extends Controller
+{
+    //
+}
